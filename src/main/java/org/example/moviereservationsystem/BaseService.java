49937e75e372696ceddd50867fc1db250c41898e
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-14 14:03:46 +0330

    feat: BaseService class

--- src/main/java/org/example/moviereservationsystem/BaseService.java
+++ src/main/java/org/example/moviereservationsystem/BaseService.java
@@ -1,4 +1,13 @@
 package org.example.moviereservationsystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BaseService {
+    @Autowired
+    private BaseDao baseDao;
+    public <T> T getById(int id, Class<T> EntityClass) {
+        return baseDao.getById(id, EntityClass);
+    }
 }
--- src/main/java/org/example/moviereservationsystem/movie/MovieService.java
+++ src/main/java/org/example/moviereservationsystem/movie/MovieService.java
@@ -1,12 +1,13 @@
 package org.example.moviereservationsystem.movie;
 
+import org.example.moviereservationsystem.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 
-public class MovieService {
+public class MovieService extends BaseService {
     @Autowired
     private MovieDao movieDao;
 }
