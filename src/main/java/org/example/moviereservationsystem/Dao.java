a70ab0d035a4b1f16e2e8a43c074a0a778305893
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-14 13:45:48 +0330

    feat: implemented getById method in Dao

--- src/main/java/org/example/moviereservationsystem/Dao.java
+++ src/main/java/org/example/moviereservationsystem/Dao.java
@@ -2,7 +2,11 @@ package org.example.moviereservationsystem;
 
 import jakarta.persistence.EntityManagerFactory;
 import lombok.Getter;
+import org.example.moviereservationsystem.movie.MovieEntity;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Repository;
@@ -17,6 +21,21 @@ public class Dao {
     public void setEntityManagerFactory(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
         this.entityManagerFactory = entityManagerFactory;
         sessionFactory = entityManagerFactory.getNativeEntityManagerFactory().unwrap(SessionFactory.class);
-
+    }
+    public <T> T getById(int id, Class<T> entityClass){
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+        T t = null;
+        try {
+            transaction = session.beginTransaction();
+            t = (T) session.get(entityClass, id);
+            transaction.commit();
+        } catch (HibernateException e){
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return t;
     }
 }
--- src/main/java/org/example/moviereservationsystem/movie/MovieDao.java
+++ src/main/java/org/example/moviereservationsystem/movie/MovieDao.java
@@ -9,20 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MovieDao extends Dao {
     public MovieEntity getMovieById(int id) {
-        Session session = getSessionFactory().openSession();
-        Transaction transaction = null;
-        MovieEntity movie = null;
-        try {
-            transaction = session.beginTransaction();
-            movie = (MovieEntity) session.get(MovieEntity.class, id);
-            transaction.commit();
-        } catch (HibernateException e){
-            if (transaction != null) transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        if (movie==null) return new MovieEntity();
+        MovieEntity movie = super.getById(id, MovieEntity.class);
+        if (movie == null) return new MovieEntity();
         return movie;
     }
 }
