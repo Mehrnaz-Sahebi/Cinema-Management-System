d828ec5b92d5634eb731343a767e486ad4918839
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-14 12:47:05 +0330

    feat: initialized Dao, UserDao, and MovieDao

--- src/main/java/org/example/moviereservationsystem/movie/MovieDao.java
+++ src/main/java/org/example/moviereservationsystem/movie/MovieDao.java
@@ -1,4 +1,27 @@
 package org.example.moviereservationsystem.movie;
 
-public class MovieDao extends {
+import org.example.moviereservationsystem.Dao;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MovieDao extends Dao {
+    public MovieEntity getMovieById(int id) {
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = null;
+        MovieEntity movie = null;
+        try {
+            transaction = session.beginTransaction();
+            movie = (MovieEntity) session.get(MovieEntity.class, id);
+            transaction.commit();
+        } catch (HibernateException e){
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return movie;
+    }
 }
