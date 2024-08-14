5b5be6befe3d9a3dee4ec99f17420e06183905cf
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-14 14:15:54 +0330

    feat: RequestNames interface

---
+++ src/main/java/org/example/moviereservationsystem/RequestNames.java
@@ -0,0 +1,11 @@
+package org.example.moviereservationsystem;
+
+public interface RequestNames {
+    public static final String USERS = "users";
+    public static final String MOVIES = "movies";
+    public static final String DIRECTORS = "directors";
+    public static final String CINEMAS = "cinemas";
+    public static final String ADDRESSES = "addresses";
+    public static final String ACTORS = "actors";
+    public static final String AUDITORIUMS = "auditoriums";
+}
--- src/main/java/org/example/moviereservationsystem/movie/MovieController.java
+++ src/main/java/org/example/moviereservationsystem/movie/MovieController.java
@@ -3,18 +3,19 @@ package org.example.moviereservationsystem.movie;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import org.example.moviereservationsystem.RequestNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("movies")
+@RequestMapping(RequestNames.MOVIES)
 public class MovieController {
-
+    public static final String SLASH_MOVIE_SLASH = "/movie/";
     @Autowired
     private MovieService movieService;
 
 
-    @GetMapping("/movie/{id}")
+    @GetMapping(SLASH_MOVIE_SLASH+"{id}")
     public MovieEntity getMovie(@PathVariable int id, HttpServletResponse response) {
         MovieEntity movie = null;
         try {
