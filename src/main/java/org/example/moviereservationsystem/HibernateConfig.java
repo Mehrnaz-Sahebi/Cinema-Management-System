f036213fb3987ddfa7e3306127d67e1dd1930428
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-14 10:01:32 +0330

    feat: configs

---
+++ src/main/java/org/example/moviereservationsystem/HibernateConfig.java
@@ -0,0 +1,48 @@
+package org.example.moviereservationsystem;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+    @Bean (name = "entityManagerFactory")
+    public LocalSessionFactoryBean hibernateSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan(  new String[] {
+                "org.example.moviereservationsystem"
+        });
+//        Properties hibernateProperties = new Properties();
+//        hibernateProperties.put("hibernate.dialect", HIBERNATE_DIALECT);
+//        hibernateProperties.put("hibernate.show_sql", HIBERNATE_SHOW_SQL);
+//        hibernateProperties.put("hibernate.hbm2ddl.auto", HIBERNATE_HBM2DDL_AUTO);
+//        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
+    @Bean HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/movie-reservation");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+}
