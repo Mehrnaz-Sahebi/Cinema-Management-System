c3f5cff6dba53cb5d91edae54d1a672dfdf04003
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-19 10:57:31 +0330

    feat: UserController initialization

---
+++ src/main/java/org/example/moviereservationsystem/user/UserController.java
@@ -0,0 +1,39 @@
+package org.example.moviereservationsystem.user;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.moviereservationsystem.RequestNames;
+import org.example.moviereservationsystem.movie.MovieEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(RequestNames.USERS)
+public class UserController {
+    public static final String SLASH_USER_SLASH = "/user/";
+    public static final String SLASH_USER = "/user";
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(SLASH_USER_SLASH +"{id}")
+    public UserEntity getUser(@PathVariable int id, HttpServletResponse response) {
+        UserEntity user = null;
+        try {
+            user = userService.getById(id);
+        } catch (EntityNotFoundException e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return user;
+    }
+    @PostMapping(SLASH_USER)
+    public UserEntity addUser(@RequestBody UserEntity user, HttpServletResponse response) {
+        UserEntity userToReturn = null;
+        try {
+            userToReturn = userService.addUser(user);
+        }catch (EntityExistsException e){
+            response.setStatus(HttpServletResponse.SC_CONFLICT);
+        } return userToReturn;
+    }
+}
