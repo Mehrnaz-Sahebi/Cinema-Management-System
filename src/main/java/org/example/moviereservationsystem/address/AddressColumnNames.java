27d9262249209d4ba2b8d869e11cd4638d2f7cf6
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-24 10:14:41 +0330

    refactor: address package

--- src/main/java/org/example/moviereservationsystem/address/AddressColumnNames.java
+++ src/main/java/org/example/moviereservationsystem/address/AddressColumnNames.java
@@ -3,7 +3,7 @@ package org.example.moviereservationsystem.address;
 public interface AddressColumnNames {
     public static final String ADDRESS_ID = "address-id";
     public static final String NUMBER = "number";
-    public static final String ALLEY= "alley";
+    public static final String ALLEY = "alley";
     public static final String STREET = "street";
     public static final String CITY = "city";
 }
--- src/main/java/org/example/moviereservationsystem/address/AddressEntity.java
+++ src/main/java/org/example/moviereservationsystem/address/AddressEntity.java
@@ -8,20 +8,26 @@ import lombok.Setter;
 import org.example.moviereservationsystem.TableNames;
 import org.example.moviereservationsystem.base.BaseEntity;
 import org.example.moviereservationsystem.cinema.CinemaEntity;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @Embeddable
 public class AddressEntity implements BaseEntity {
+
     @Column(name = AddressColumnNames.ADDRESS_ID, nullable = false)
     private int id;
+
     @Column(name = AddressColumnNames.NUMBER)
     private String number;
+
     @Column(name = AddressColumnNames.ALLEY)
     private String alley;
+
     @Column(name = AddressColumnNames.STREET)
     private String street;
+
     @Column(name = AddressColumnNames.CITY)
     private String city;
 }
