38b7c552bc22cc05961b2dd5d394c4c34c46cdac
Mehrnaz <mehrnazsa83@gmail.com>
2024-08-24 10:27:41 +0330

    refactor: authenication package

--- src/main/java/org/example/moviereservationsystem/authentication/AuthenticationRequest.java
+++ src/main/java/org/example/moviereservationsystem/authentication/AuthenticationRequest.java
@@ -10,5 +10,4 @@ import lombok.Setter;
 public class AuthenticationRequest {
     private String phoneNumber;
     private String password;
-
 }
--- src/main/java/org/example/moviereservationsystem/authentication/jwt/JwtAuthFilter.java
+++ src/main/java/org/example/moviereservationsystem/authentication/jwt/JwtAuthFilter.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -27,26 +28,27 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtils jwtUtils;
     public static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthFilter.class);
+
     //TODO doesn't accept requests from browser
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String userPhoneNumber;
         final String jwtToken;
-        if (authHeader == null || !authHeader.startsWith("Bearer")){
-            filterChain.doFilter(request,response);
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
             return;
         }
         jwtToken = authHeader.substring(7);
         userPhoneNumber = jwtUtils.extractUsername(jwtToken);
-        if(userPhoneNumber!=null && SecurityContextHolder.getContext().getAuthentication() == null){
+        if (userPhoneNumber != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserEntityDetails userEntityDetails = (UserEntityDetails) userService.loadUserByUsername(userPhoneNumber);
-            if (jwtUtils.isTokenValid(jwtToken, userEntityDetails)){
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userEntityDetails,null, userEntityDetails.getAuthorities());
+            if (jwtUtils.isTokenValid(jwtToken, userEntityDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userEntityDetails, null, userEntityDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
 }
